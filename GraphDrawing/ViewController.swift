d24c2d34b64a5444a4a6bbc36928ddf58cf81f7d
Danny Shmueli <dannyshmueli@gmail.com>
2016-03-22 23:17:13 +0200

    moved everything to viewDidLoad - since using the layout subviews we can do this

--- GraphDrawing/ViewController.swift
+++ GraphDrawing/ViewController.swift
@@ -14,30 +14,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var graphWrapperView: UIView!
     
     
-    let weightArray = [80.1, 77.3, 78.5, 84.7, 78.2, 79.9, 78.4, 76.8, 72.4, 74.9]
-    let carbArray   = [52, 27, 56, 68, 39, 76, 53, 42, 32, 53]
-    let dateArray   = ["20/3", "21/3", "22/3", "23/3", "24/3", "25/3", "26/3", "27/3", "28/3", "29/3"]
+    let weightArray = [80.1, 77.3, 78.5, 84.7, 78.2, 79.9, 78.4, 76.8, 72.4, 74.9, 80.1, 77.3, 78.5, 84.7, 78.2, 79.9, 78.4, 76.8, 72.4, 74.9]
+    let carbArray   = [52, 27, 56, 68, 39, 76, 53, 42, 32, 53, 52, 27, 56, 68, 39, 76, 53, 42, 32, 53]
+    let dateArray   = ["20/3", "21/3", "22/3", "23/3", "24/3", "25/3", "26/3", "27/3", "28/3", "29/3", "20/3", "21/3", "22/3", "23/3", "24/3", "25/3", "26/3", "27/3", "28/3", "29/3"]
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        
+    override func viewDidLoad() {
+        super.viewDidLoad()
         let graphView = LineAndBarGraphView(frame: self.graphWrapperView.bounds)
         graphView.lineGraphData = weightArray
         graphView.barGraphData = carbArray
         graphView.dateArray = dateArray
         
         graphView.autoresizingMask = [ .FlexibleWidth , .FlexibleHeight]
-
+        
         self.graphWrapperView.addSubview(graphView)
         graphView.loadGraphFromPoints()
-        
     }
-    
 }
 
